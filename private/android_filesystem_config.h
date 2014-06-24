020a11815ef7f6b266ddab435ae58850959201e2
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2014-06-24 12:09:27 -0700

    system: core: Add Sensors group
    Defining group for sensors.
    
    CRs-Fixed: 431081
    
    Change-Id: I58151f2fd727bded1b16adb3caeac85e27e44036
    
    Conflicts:
    
    	private/android_filesystem_config.h

--- private/android_filesystem_config.h
+++ private/android_filesystem_config.h
@@ -116,6 +116,8 @@
 #define AID_MOT_DLNA      9011  /* DLNA native */
 #endif // MOTOROLA_UIDS
 
+#define AID_SENSORS       3012 /* access to /dev/socket/sensor_ctl_socket & QCCI/QCSI */
+
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
@@ -210,7 +212,7 @@ static const struct android_id_info android_ids[] = {
     { "mot_caif",  AID_MOT_CAIF, },
     { "mot_dlna",  AID_MOT_DLNA, },
 #endif
-
+    { "sensors",       AID_SENSORS, },
     { "everybody",     AID_EVERYBODY, },
     { "misc",          AID_MISC, },
     { "nobody",        AID_NOBODY, },
